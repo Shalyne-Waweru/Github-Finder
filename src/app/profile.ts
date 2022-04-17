3b4863b9eda4b2a60f1eb84644b0f932140ed551
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-17 19:06:30 +0300

    Create a user profile class

---
+++ src/app/profile.ts
@@ -0,0 +1,13 @@
+export class Profile {
+  //Here we make Angular sieve out what we need from the API
+  constructor(
+    public avatar_url:string,
+    public html_url:string,
+    public login:string,
+    public followers:number,
+    public following:number,
+    public public_repos:number,
+    public created_at:string ){
+      
+  }
+}
