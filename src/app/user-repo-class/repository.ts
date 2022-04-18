ff44944e9f48b261cd82af871bb4f209c8fce959
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-18 18:26:58 +0300

    Create a user repository class

---
+++ src/app/user-repo-class/repository.ts
@@ -0,0 +1,9 @@
+export class Repository {
+  //Here we make Angular sieve out what we need from the API
+  constructor(
+    public name:string,
+    public description:string,
+    public html_url:string){
+
+  }
+}
