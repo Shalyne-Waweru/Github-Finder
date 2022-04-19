39977d413e82acda9bd70281d0fd6151f28eb016
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-19 16:03:31 +0300

    Implement the date-count pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Repository } from './user-repo-class/repository';
 
 @Pipe({
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
+  repos:Repository;
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+ transform(value: any): any {
+  let valueArr = value.replace(/(\d{4})-(\d{2})-(\d{2}).*/ig,"$1 $2 $3").split(" ");
+  let dateCreated = new Date(valueArr[0],valueArr[1]-1, valueArr[2]);
+  let now = new Date() ;
+  let diff = now.getTime() - dateCreated.getTime();
+  diff/=(1000 * 60 * 60 * 24);
+  return Math.floor(diff);
+}
 
 }
