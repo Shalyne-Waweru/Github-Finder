ad2cf9c91f898e883e3e2677239476b428cc8acf
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-20 01:48:24 +0300

    Add routes to the app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent},
+  { path: 'search', component: UserProfileComponent},
+  { path: '', redirectTo:"/home", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
