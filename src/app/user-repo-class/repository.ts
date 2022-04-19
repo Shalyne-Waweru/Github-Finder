9ca64e739568ccfa9059fb132547297f303b32fc
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-19 14:39:34 +0300

    Display the repositories' updated dates

--- src/app/user-repo-class/repository.ts
+++ src/app/user-repo-class/repository.ts
@@ -3,7 +3,8 @@ export class Repository {
   constructor(
     public name:string,
     public description:string,
-    public html_url:string){
+    public html_url:string,
+    public updated_at:Date){
 
   }
 }
